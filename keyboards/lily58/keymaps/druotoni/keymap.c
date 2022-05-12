da5b27868cd63678e446ca03181ede32c026d04f
Valerian Ratu <leri.ratu@gmail.com>
2022-05-11 23:26:52 -0700

    feat: finalize backspace placement, cleanup keycodes

--- keyboards/lily58/keymaps/druotoni/keymap.c
+++ keyboards/lily58/keymaps/druotoni/keymap.c
@@ -5,6 +5,8 @@
 #include "keymap_french.h"
 #include "transactions.h"
 
+
+
 // global
 #include "gui_state.h"
 #include "boot.h"
@@ -20,6 +22,15 @@
 // right side
 #include "ring.h"
 
+#define ESC_CTL LCTL_T(KC_ESC)
+#define LOWER MO(_LOWER)
+#define RAISE MO(_RAISE)
+#define ADJST MO(_ADJUST)
+#define ENT_SFT LSFT_T(KC_ENT)
+#define BSP_SFT LSFT_T(KC_BSPC)
+
+// The dedicated square bracket keys are a little bit wasteful :>
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* QWERTY
@@ -32,17 +43,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |ADJ|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LALT  |LGUI  |LOWER |/Space  /       \Enter/ \   |RAISE | BCK   |   DEL|
+ *                   | LALT  |LGUI  |LOWER |/Space  /       \BSPC / \   |RAISE | ENTER |   DEL|
  *                   |      |      |      |/       /         \ SHFT  \ |      |       |      |
  *                   `----------------------------'           '------''--------------------'
  */
 
  [_QWERTY] = LAYOUT(
-  KC_ESC,            KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
-  KC_TAB,            KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
-  LCTL_T(KC_ESC),  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT,           KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,       KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MO(_ADJUST),
-                                      KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC, LSFT_T(KC_ENT), MO(_RAISE), KC_BSPC, KC_DEL
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
+  ESC_CTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,       KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, ADJST,
+                             KC_LALT, KC_LGUI, LOWER, KC_SPC,       BSP_SFT, RAISE, KC_ENT, KC_DEL
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
